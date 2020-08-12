31b91eb187c0f7641092ba0f01464930345fc082
Yuriy Kondrashov <32159957+axisonliner@users.noreply.github.com>
2020-08-12 14:43:01 +0300

    Update del_files.py
    added description to file

--- del_files.py
+++ del_files.py
@@ -1,8 +1,14 @@
+# Script for deleting files from a folder using the command line:
+# command line: python del_files.py arg1 arg2
+# - arg1 = folder path
+# - arg2 = file extention
+# Example: python del_files.py C:\Users\y.kondrashov\Desktop\ep0001 .exr
+
 import os
 import sys
 
 directory = str(sys.argv[1])
-# directory = "C:/Users/y.kondrashov/Desktop/ep0001/"
+# directory = "C:/Desktop/ep0001/"
 
 for root, dirs, files in os.walk(directory):
     path = root.split(os.sep)
