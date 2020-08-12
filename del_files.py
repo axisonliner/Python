233e4a38a1e8f30b568637275d43868deafb8b9a
Yuriy Kondrashov <32159957+axisonliner@users.noreply.github.com>
2020-08-12 15:01:19 +0300

    Update del_files.py
    added exception

--- del_files.py
+++ del_files.py
@@ -7,14 +7,27 @@
 import os
 import sys
 
-directory = str(sys.argv[1])
-# directory = "C:/Desktop/ep0001/"
 
-for root, dirs, files in os.walk(directory):
-    path = root.split(os.sep)
-    for file in files:
-    	if file.endswith(str(sys.argv[2])):
-    		path_to_file = '/'.join(path) + '/' + file
-    		print("delete file: {}".format(path_to_file))
-    		os.remove(path_to_file)
-        
+def del_files(path, file):
+    try:
+        directory = str(sys.argv[1])
+        for root, dirs, files in os.walk(directory):
+            path = root.split(os.sep)
+            for file in files:
+                if file.endswith(str(sys.argv[2])):
+                    path_to_file = '/'.join(path) + '/' + file
+                    print("delete file: {}".format(path_to_file))
+                    os.remove(path_to_file)
+    
+    except IndexError:
+        print("Please, please provide two arguments")
+        print("command line: python del_files.py arg1 arg2")
+        print("arg1 = folder path")
+        print("arg2 = file extention")
+        print("Example: python del_files.py C:/Desktop/folder_name .exr")
+
+if __name__ == '__main__':
+    path = ""
+    file = ""
+    print
+    del_files(path, file)
